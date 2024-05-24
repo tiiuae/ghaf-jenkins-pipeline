39a47d0ffebbadab5b10ccb16453aa48770ebf33
Joonas Rautiola <joonas.rautiola@unikie.com>
2024-05-24 10:23:54 +0300

    Make provenance compliant with new buildtype document
    Signed-off-by: Joonas Rautiola <joonas.rautiola@unikie.com>

---
+++ provenance-external-params.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# SPDX-FileCopyrightText: 2022-2024 TII (SSRC) and the Ghaf contributors
+# SPDX-License-Identifier: Apache-2.0
+
+# details of the target being built.
+# TODO: Target name should be populated from jenkins, but currently that's
+# not possible since the environment is only generated once instead of being
+# unique for every target
+target=$(jq -n \
+    --arg name "?" \
+    --arg repository \
+    "$URL" --arg ref \
+    "$BRANCH" \
+    '$ARGS.named')
+
+# details of the workflow/pipeline repository
+workflow=$(jq -n \
+    --arg name "$JOB_NAME" \
+    --arg repository "$GIT_URL" \
+    --arg ref "$GIT_COMMIT" \
+    '$ARGS.named')
+
+# final external parameters is assembled
+jq -n \
+    --argjson target "$target" \
+    --argjson workflow "$workflow" \
+    --arg job "$JOB_NAME" \
+    --arg buildRun "$BUILD_ID" \
+    '$ARGS.named'
