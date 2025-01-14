899a3bbe69aadfdd4bd0ba7d7024d438ced3abc1
Samuli Leivo <samuli.leivo@unikie.com>
2025-01-14 10:10:08 +0200

    Launch gui test suite only for lenovo-x1
    Signed-off-by: Samuli Leivo <samuli.leivo@unikie.com>

--- ghaf-hw-test.groovy
+++ ghaf-hw-test.groovy
@@ -235,7 +235,7 @@ pipeline {
       }
     }
     stage('GUI test') {
-      when { expression { env.BOOT_PASSED == 'true' && env.TESTSET.contains('_gui_')} }
+      when { expression { env.BOOT_PASSED == 'true' && params.DEVICE_CONFIG_NAME == "lenovo-x1" && env.TESTSET.contains('_gui_')} }
       steps {
         script {
           ghaf_robot_test('gui')
