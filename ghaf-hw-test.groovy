81609730a3d28a9f04050bcd390a48948771fb0c
Fayad Fami <fayad.fami@tii.ae>
2025-08-25 09:08:01 +0000

    Disable provenance stage for uae release test
    Signed-off-by: Fayad Fami <fayad.fami@tii.ae>

--- ghaf-hw-test.groovy
+++ ghaf-hw-test.groovy
@@ -116,36 +116,36 @@ pipeline {
         }
       }
     }
-    stage('Verify provenance') {
-      when { expression { params.containsKey('PROVENANCE_URL') && params.PROVENANCE_URL != "null" } }
-      steps {
-        script {
-          sh "rm -fr ${TMP_PROVENANCE_DIR}"
-          // Wget occasionally fails due to a failure in name lookup. Below is a
-          // hack to force re-try a few times before aborting. Wget options, such
-          // as --tries, --waitretry, --retry-connrefused, etc. do not help in case
-          // the failure is due to an issue in name resolution which is considered
-          // a fatal error. Therefore, we need to add the below retry loop.
-          // TODO: remove the below re-try loop when test network DNS works
-          // reliably.
-          sh """
-            retry=1
-            max_retry=3
-            while ! wget -nv --show-progress --progress=dot:giga -P ${TMP_PROVENANCE_DIR} ${params.PROVENANCE_URL};
-            do
-              if (( \$retry >= \$max_retry )); then
-                echo "wget failed after \$retry retries"
-                exit 1
-              fi
-              retry=\$(( \$retry + 1 ))
-              sleep 5
-            done
-          """
-          sh "wget -nv -P ${TMP_PROVENANCE_DIR} ${params.PROVENANCE_URL}.sig"
-          sh "policy-checker ${TMP_PROVENANCE_DIR}/provenance.json --sig ${TMP_PROVENANCE_DIR}/provenance.json.sig --policy ${TRUST_POLICY_PATH}"
-        }
-      }
-    }
+//    stage('Verify provenance') {
+//      when { expression { params.containsKey('PROVENANCE_URL') && params.PROVENANCE_URL != "null" } }
+//      steps {
+//        script {
+//          sh "rm -fr ${TMP_PROVENANCE_DIR}"
+//          // Wget occasionally fails due to a failure in name lookup. Below is a
+//          // hack to force re-try a few times before aborting. Wget options, such
+//          // as --tries, --waitretry, --retry-connrefused, etc. do not help in case
+//          // the failure is due to an issue in name resolution which is considered
+//          // a fatal error. Therefore, we need to add the below retry loop.
+//          // TODO: remove the below re-try loop when test network DNS works
+//          // reliably.
+//          sh """
+//            retry=1
+//            max_retry=3
+//            while ! wget -nv --show-progress --progress=dot:giga -P ${TMP_PROVENANCE_DIR} ${params.PROVENANCE_URL};
+//            do
+//              if (( \$retry >= \$max_retry )); then
+//                echo "wget failed after \$retry retries"
+//                exit 1
+//              fi
+//              retry=\$(( \$retry + 1 ))
+//              sleep 5
+//            done
+//          """
+//          sh "wget -nv -P ${TMP_PROVENANCE_DIR} ${params.PROVENANCE_URL}.sig"
+//          sh "policy-checker ${TMP_PROVENANCE_DIR}/provenance.json --sig ${TMP_PROVENANCE_DIR}/provenance.json.sig --policy ${TRUST_POLICY_PATH}"
+//        }
+//      }
+//    }
     stage('Image download') {
       steps {
         script {
