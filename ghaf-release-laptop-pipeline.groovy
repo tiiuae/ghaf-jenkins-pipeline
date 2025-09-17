e4638f58e059b569696b72b72efcc60f037c1d54
Ville-Pekka Juntunen <ville-pekka.juntunen@unikie.com>
2025-09-17 11:11:19 +0300

    Add ghaf-release-laptop-pipeline
    Signed-off-by: Ville-Pekka Juntunen <ville-pekka.juntunen@unikie.com>

---
+++ ghaf-release-laptop-pipeline.groovy
@@ -0,0 +1,123 @@
+#!/usr/bin/env groovy
+
+// SPDX-FileCopyrightText: 2022-2024 TII (SSRC) and the Ghaf contributors
+// SPDX-License-Identifier: Apache-2.0
+
+////////////////////////////////////////////////////////////////////////////////
+
+def REPO_URL = 'https://github.com/tiiuae/ghaf/'
+def WORKDIR  = 'ghaf'
+def DEF_GITREF = 'main'
+
+// Defines if there is need to run purge_artifacts
+def purge_stashed_artifacts = true
+
+// Utils module will be loaded in the first pipeline stage
+def utils = null
+
+properties([
+  githubProjectProperty(displayName: '', projectUrlStr: REPO_URL),
+  parameters([
+    string(name: 'GITREF', defaultValue: DEF_GITREF, description: 'Ghaf git reference (Commit/Branch/Tag)')
+  ])
+])
+
+target_jobs = [:]
+
+////////////////////////////////////////////////////////////////////////////////
+
+def targets = [
+  // docs
+  [ system: "x86_64-linux",
+    target: "doc",
+    archive: false,
+    scs: false,
+    hwtest_device: null,
+  ],
+
+  // lenovo x1
+  [ system: "x86_64-linux",
+    target: "lenovo-x1-carbon-gen11-debug",
+    archive: true,
+    scs: true,
+    hwtest_device: "lenovo-x1",
+  ],
+  [ system: "x86_64-linux",
+    target: "lenovo-x1-carbon-gen11-debug-installer",
+    archive: true,
+    scs: true,
+    hwtest_device: null,
+  ],
+
+  // DarterPRO System76
+  [ system: "x86_64-linux",
+    target: "system76-darp11-b-debug",
+    archive: true,
+    scs: true,
+    hwtest_device: "darter-pro",
+  ],
+]
+
+////////////////////////////////////////////////////////////////////////////////
+
+pipeline {
+  agent { label 'built-in' }
+  options {
+    timestamps ()
+    buildDiscarder(logRotator(numToKeepStr: '100'))
+  }
+  environment {
+    // https://stackoverflow.com/questions/46680573
+    GITREF = params.getOrDefault('GITREF', DEF_GITREF)
+  }
+  stages {
+    stage('Checkout') {
+      steps {
+        script { utils = load "utils.groovy" }
+        dir(WORKDIR) {
+          checkout scmGit(
+            branches: [[name: env.GITREF]],
+            extensions: [[$class: 'WipeWorkspace']],
+            userRemoteConfigs: [[url: REPO_URL]]
+          )
+          script {
+            env.TARGET_REPO = sh(script: 'git remote get-url origin', returnStdout: true).trim()
+            env.TARGET_COMMIT = sh(script: 'git rev-parse HEAD', returnStdout: true).trim()
+            env.ARTIFACTS_REMOTE_PATH = "${env.JOB_NAME}/build_${env.BUILD_ID}-commit_${env.TARGET_COMMIT}"
+          }
+        }
+      }
+    }
+
+    stage('Evaluate') {
+      steps {
+        dir(WORKDIR) {
+          lock('evaluator') {
+            script {
+              utils.nix_eval_jobs(targets)
+              target_jobs = utils.create_parallel_stages(targets, testset='_relayboot_bat_')
+            }
+          }
+        }
+      }
+    }
+
+    stage('Build targets') {
+      steps {
+        script {
+          parallel target_jobs
+        }
+      }
+    }
+ }
+  post {
+    always {
+      script {
+        if(purge_stashed_artifacts) {
+          // Remove build results if those are older than 90 days
+          utils.purge_artifacts_by_age('ghaf-release-pipeline', '90d')
+        }
+      }
+    }
+  }
+}
